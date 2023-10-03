f6aed0ef6d590ca0b4a40db4ff9f4229d68f7eaf
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-03 19:11:28 +0200

    removed argv and added parsed string

--- src/executer/errors.c
+++ src/executer/errors.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 16:56:52 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/09/19 17:37:38 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/03 19:10:00 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@
 // {
 // 	return (error_code);
 // }
-int	ft_print_command_error(char **argv, int error_code)
+int	ft_print_command_error(char **parsed, int error_code, int i)
 {
-	ft_putstr_fd(&argv[0][2], 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(argv[2], 2);
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(parsed[i], 2);
 	ft_putstr_fd(": command not found\n", 2);
 	return (error_code);
 }
