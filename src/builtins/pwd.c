a5aac0928de496df2a363a0b1638eb2a45c447c6
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-03 13:11:05 +0200

    norm changes and removed unsused include

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,13 +6,12 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 18:20:46 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/10/02 18:25:45 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/03 13:10:57 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../tools/ft_printf/ft_printf.h"
 #include <unistd.h>
-// #include <linux/limits.h>
 #include <limits.h>
 
 int	ft_cwd(void)
@@ -20,10 +19,10 @@ int	ft_cwd(void)
 	char	path[PATH_MAX];
 
 	if (getcwd(path, PATH_MAX))
-		{
-			ft_printf("%s\n", path);
-			return (0);
-		}
+	{
+		ft_printf("%s\n", path);
+		return (0);
+	}
 	else
 		return (1);
 }
