cf2ec4eea62f8402b01215fb25d819501f1db287
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-03 21:42:59 +0200

    changed declarations and added new data to execute struct

--- src/executer/pipex.h
+++ src/executer/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 10:18:35 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/10/02 08:56:12 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/03 19:18:03 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,19 @@
 # include "../inc/minishell.h"
 
 typedef struct s_execute {
-	int		here_doc;
+	char	*input;
+	int		append;
+	char	*output;
+	char	*limiter;
+	int		count_commands;
+	int		count_builtins;
 	int		count_children;
 	int		*id;
 	int		count_pipes;
 	int		**pipe_fd;
 	int		error;
-	int		argc;
 	char	**commands;
 	char	**envp;
-	char	**argv;
 }	t_execute;
 
 char	*ft_check_command_and_get_path(char *command, char **envp);
@@ -41,15 +44,14 @@ char	**ft_get_command_arg_array(char *command);
 void	ft_remove(char *start, char *end);
 void	ft_close_fds(t_execute *exec, int current_child);
 void	ft_close_all_fds(t_execute *exec);
-int		ft_print_command_error(char **argv, int error_code);
-int		ft_init_struct(t_execute *new, int argc, char **argv, char **envp);
+int		ft_print_command_error(char **parsed, int error_code, int i);
+int		ft_init_struct(t_execute *new, int *types, char **parsed, char **envp);
 void	ft_free_data(t_execute *exec);
-int		ft_init(t_execute *exec, int argc, char **argv, char **envp);
 int		ft_forking(t_execute *exec);
 int		ft_child(int i, t_execute *exec);
 int		ft_parent(t_execute *exec);
 char	*ft_remove_slash(char *deleted);
 int		ft_strchr_count(const char *string, char c);
-int	execute(int argc, char **argv, char **envp);
+int		ft_init(t_execute *exec, int *types, char **parsed, char **envp);
 
 #endif
