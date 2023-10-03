3e85c009e8e73b2869dc6b75df7841326be07668
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-03 21:37:27 +0200

    added test cases for execute

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 14:19:04 by nschutz           #+#    #+#             */
-/*   Updated: 2023/10/02 14:52:44 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/03 21:34:12 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,36 +44,36 @@ int	main(int argc, char **argv, char **envp)
 	sa.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa, NULL);
 	signal(SIGQUIT, SIG_IGN);
-	rl_replace_line("hello", 1);
-	rl_redisplay();
+	// rl_replace_line("hello", 1);
+	// rl_redisplay();
 	line = readline(0);
-	ft_putstr_fd("hello\n", 1);
-	char *testargv[] = {"minishell", "input.txt", "cat", "output.txt", NULL};
-	execute(4, testargv, envp);
-	// while (1)
-	// {
-	// 	if (!line)
-	// 	{
-	// 		write(0 , "exit\n", 6);
-	// 		// rl_on_new_line();
-	// 		// rl_replace_line("exit", 0);
-	// 	}
-	// 	if (line && !ft_strncmp(line, "exit", 4))
-	// 		break ;
-	// 	// printf("minishell-0.1$ ");
-	// 	line = readline(0);
-	// 	if (!line)
-	// 		add_history("");
-	// 	else
-	// 		add_history(line);
-	// }
-	
-	// sigaction, sigemptyset, sigaddset
-	// signal
-	//input_sort(line, envp);
-	//free(line);
-	rl_replace_line("hello", 1);
-	rl_redisplay();
-	line = readline(0);
+	// ft_putstr_fd("hello\n", 1);
+
+	// char *testargv[] = {"input.txt", "cat -e", "cat -e", "grep a", "cat -e", NULL};
+	// int	testintargv[] = {1, 5, 6, 5, 5, NULL};
+	// char *testargv[] = {"cat -e", "cat -e", "grep a", "cat -e", "output.txt", NULL};
+	// int	testintargv[] = {5, 6, 5, 5, 3, NULL};
+	// char *testargv[] = {"cat -e", "cat -e", "grep a", "cat -e", "output.txt", NULL};
+	// int	testintargv[] = {5, 6, 5, 5, 4, NULL};
+	// char *testargv[] = {"ls -a", "cat -e", "grep a", "cat -e", NULL};
+	// int	testintargv[] = {5, 6, 5, 5, NULL};
+	char *testargv[] = {"ls -a", "cat -e", "grep a", "cat -e", "output.txt", NULL};
+	int	testintargv[] = {5, 6, 5, 5, 3, NULL};
+	// char *testargv[] = {"input.txt", "cat -e", "cat -e", "grep a", "cat -e", "output.txt", NULL};
+	// int	testintargv[] = {1, 5, 6, 5, 5, 4, NULL};
+	// char *testargv[] = {"cat -e", "echo hallo", "wc -l", NULL};
+	// int	*testintargv[] = {5, 6, 5, NULL};
+	// char *testargv[] = {"end", "cat -e", "echo hallo", "wc -l", "output.txt", NULL};
+	// int	*testintargv[] = {2, 5, 6, 5, 4, NULL};
+	// input 1
+	// limiter 2
+	// output 3
+	// append 4
+	// command 5
+	// builtin 6
+	execute(testintargv, testargv, envp);
+	// rl_replace_line("hello", 1);
+	// rl_redisplay();
+	// line = readline(0);
 	return (0);
 }
