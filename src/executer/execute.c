358673dda6cdab81843757f779e6d307078b40ed
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-04 13:55:09 +0200

    implemented one builting execution in parent, with right piping and duping

--- src/executer/execute.c
+++ src/executer/execute.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 19:32:24 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/10/03 21:08:09 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/04 12:02:18 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,62 @@ int	ft_parent(t_execute *exec)
 	int	i;
 	int	status;
 
-	// if (exec.builtin == 1)
-	// 	ft_
-	ft_close_all_fds(exec);
+	i = -1;
+	if (exec->count_builtins == 1)
+	{
+		printf("one and only parent builtin\n");
+		close(exec->pipe_fd[0][0]);
+		close(exec->pipe_fd[0][1]);
+		if (exec->input)
+		{
+			printf("with input file\n");
+			exec->pipe_fd[exec->count_pipes - 1][0]
+				= open(exec->input, O_RDONLY);
+			if (exec->pipe_fd[exec->count_pipes - 1][0] < 1)
+			{
+				perror("Error");
+				return (1);
+			}
+			else
+				exec->pipe_fd[0][0] = 0;
+		}
+		if (exec->output)
+		{
+			printf("with output file\n");
+			if (exec->append)
+				exec->pipe_fd[0][1] = open(exec->output, O_RDWR
+						| O_CREAT | O_APPEND, 0644);
+			else
+				exec->pipe_fd[0][1] = open(exec->output, O_RDWR
+						| O_CREAT | O_TRUNC, 0644);
+			if (exec->pipe_fd[0][1] < 1)
+			{
+				perror("Error");
+				return (1);
+			}
+		}
+		else
+			exec->pipe_fd[0][1] = 1;
+		dup2(exec->pipe_fd[0][0], 0);
+		dup2(exec->pipe_fd[0][1], 1);
+		if (!ft_strncmp(exec->commands[0], "echo", 4))
+			ft_echo(exec->commands);						// need to get the right stuff
+		// else if (!ft_strncmp(exec->commands[0], "cd", 4))
+		// 	ft_cd(exec->commands[0]);
+		else if (!ft_strncmp(exec->commands[0], "pwd", 4))
+			ft_pwd();
+		// else if (!ft_strncmp(exec->commands[0], "export", 4))
+		// 	ft_export(exec->commands[0]);
+		// else if (!ft_strncmp(exec->commands[0], "unset", 4))
+		// 	ft_unset(exec->commands[0]);
+		else if (!ft_strncmp(exec->commands[0], "env", 4))
+			ft_env(exec->envp);
+		// else if (!ft_strncmp(exec->commands[0], "exit", 4))
+		// 	ft_exit(exec->commands[0]);
+		ft_close_fds(exec, 0);
+	}
+	else
+		ft_close_all_fds(exec);
 	i = -1;
 	while (++i < exec->count_children)
 	{
@@ -39,6 +92,7 @@ int	ft_child(int i, t_execute *exec)
 	char	**command_array;
 	char	*command;
 
+	printf("child %i executing: %s\n", i, exec->commands[i]);
 	ft_close_fds(exec, i);
 	if (i == 0)
 		dup2(exec->pipe_fd[exec->count_pipes - 1][0], 0);
@@ -48,7 +102,7 @@ int	ft_child(int i, t_execute *exec)
 		dup2(exec->pipe_fd[0][1], 1);
 	else
 		dup2(exec->pipe_fd[i][1], 1);
-	printf("exec->commands[%i] to split: %s", i, exec->commands[i]);
+	dprintf(2, "exec->commands[%i] to split: %s", i, exec->commands[i]);
 	command_array = ft_get_command_arg_array
 		(exec->commands[i]);
 	command = ft_check_command_and_get_path(command_array[0], exec->envp);
@@ -113,7 +167,6 @@ int	ft_init(t_execute *exec, int *types, char **parsed, char **envp)
 	int	i;
 
 	i = -1;
-	// 
 	ft_init_struct(exec, types, parsed, envp);
 	if (exec->id == NULL || exec->pipe_fd == NULL)
 		return (1);
