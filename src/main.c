7263ddd6c478194bc5034bd5d77155affd6ca5a3
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-04 22:22:02 +0200

    just test changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 14:19:04 by nschutz           #+#    #+#             */
-/*   Updated: 2023/10/04 15:54:35 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/04 18:38:25 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,45 @@
 #include <stdio.h>
 #include "executer/pipex.h"
 
-void	ft_sig_handle(int sig)
-{
-	if (sig == 2)
-	{
-		// rl_replace_line("", 0);
-		// printf("signal: %i caught! i will be a new promt on new line\n", sig);
-		// printf("minishell after signal2-0.1$ ");
-		// 
-		rl_replace_line("hello\n", 0);
-		// write(0, "\n", 2);
-		rl_on_new_line();
-		rl_redisplay();
-		// readline, rl_clear_history, rl_on_new_line,
-		// rl_replace_line, rl_redisplay
-	}
-	else
-		printf("signal: %i caught!\n", sig);
-}
+// void	ft_sig_handle(int sig)
+// {
+// 	if (sig == 2)
+// 	{
+// 		// rl_replace_line("", 0);
+// 		// printf("signal: %i caught! i will be a new promt on new line\n", sig);
+// 		// printf("minishell after signal2-0.1$ ");
+// 		// 
+// 		rl_replace_line("hello\n", 0);
+// 		// write(0, "\n", 2);
+// 		rl_on_new_line();
+// 		rl_redisplay();
+// 		// readline, rl_clear_history, rl_on_new_line,
+// 		// rl_replace_line, rl_redisplay
+// 	}
+// 	else
+// 		printf("signal: %i caught!\n", sig);
+// }
 
 int	main(int argc, char **argv, char **envp)
 {
-	struct sigaction	sa;
+	// struct sigaction	sa;
 	char				*line;
 
-	sa.sa_handler = &ft_sig_handle;
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa, NULL);
-	signal(SIGQUIT, SIG_IGN);
+	// sa.sa_handler = &ft_sig_handle;
+	// sa.sa_flags = SA_RESTART;
+	// sigaction(SIGINT, &sa, NULL);
+	// signal(SIGQUIT, SIG_IGN);
 	// rl_replace_line("hello", 1);
 	// rl_redisplay();
 	line = readline(0);
-	while (line)
-	{
+	// rl_redisplay();
+	// add_history(line);
+	// while (line)
+	// {
 		input_sort(line, envp);
-		line = readline(0);
-	}
+	// 	line = readline(0);
+	// 	rl_redisplay();
+	// 	add_history(line);
+	// }
 	return (0);
 }
