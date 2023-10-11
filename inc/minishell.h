e761ecae758ffad4741dcc4254274e58638ca347
nschutz <nschutz@student.42wolfsburg.de>
2023-10-11 14:31:13 +0200

    feat: modified stringjoin now norminette

--- inc/minishell.h
+++ inc/minishell.h
@@ -67,7 +67,7 @@ int		is_file(char *s);
 int		sort_array(t_input **input, t_array *array);
 int		nv_after_red(t_input **input, int i);
 char	**mod_split(char const *s, char c);
-char	*mod_strjoin(char const *s1, char const *s2);
+char	*mod_strjoin(char *s1, char *s2);
 char	**dup_array(char **array);
 void	free_array(char **array);
 void	free_list(t_input **input);
--- src/modified/modified_strjoin_func.c
+++ src/modified/modified_strjoin_func.c
@@ -12,27 +12,33 @@
 
 #include "minishell.h"
 
-char	*mod_strjoin(char const *s1, char const *s2)
+char	*string_one(char *s1, char *joined)
+{
+	int	i;
+
+	i = 0;
+	while (s1 && s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	if (i > 0)
+		joined[i] = ' ';
+	return (joined);
+}
+
+char	*mod_strjoin(char *s1, char *s2)
 {
 	char	*joined;
 	int		i;
 	int		j;
 
-	i = 0;
 	j = 0;
 	joined = ft_calloc((ft_strlen(s1) + ft_strlen(s2)) + 2, 1);
 	if (joined)
 	{
-		while (s1 && s1[i])
-		{
-			joined[i] = s1[i];
-			i++;
-		}
-		if (i > 0)
-		{
-			joined[i] = ' ';
-			i++;
-		}
+		joined = string_one(s1, joined);
+		i = (int)ft_strlen(joined) + 1;
 		while (s2 && s2[j])
 		{
 			joined[i] = s2[j];
