0743513f54b8270bbf6d24f4685842d3e5646eb5
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-16 16:57:55 +0200

    added another free and changed definition

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,14 +6,15 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 08:41:17 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/10/10 06:59:41 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/16 16:11:18 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_exit(char **array)
+void	ft_exit(char **array, t_execute *exec)
 {
 	ft_free_array(array);
+	ft_free_array(exec->commands);
 	exit(0);
 }
