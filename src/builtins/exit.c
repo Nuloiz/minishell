4c7a2bc8c83fadcc032fa96fc28bb47a5c1c03c4
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-16 18:11:51 +0200

    added closing all fds

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 08:41:17 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/10/16 16:11:18 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/16 17:37:13 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ void	ft_exit(char **array, t_execute *exec)
 {
 	ft_free_array(array);
 	ft_free_array(exec->commands);
+	ft_close_all_fds(exec);
 	exit(0);
 }
