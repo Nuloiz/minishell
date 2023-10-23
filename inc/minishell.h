0b2d89c5b1f7349eaf304305693bed6ced75165b
nschutz <nschutz@student.42wolfsburg.de>
2023-10-23 12:26:58 +0200

    feat: making norminette happy, for the header file

--- inc/minishell.h
+++ inc/minishell.h
@@ -31,14 +31,14 @@
 // # include "../src/executer/pipex.h"
 # include <errno.h>
 
-#define COMMAND 1
-#define FLAGS	2
-#define FILE	3
-#define ENV_VAR	4
-#define PIPE	5
-#define REDIRECT 6
-#define BUILTIN	 7
-#define PARAM	 8
+# define COMMAND 1
+# define FLAGS	2
+# define FILE	3
+# define ENV_VAR	4
+# define PIPE	5
+# define REDIRECT 6
+# define BUILTIN	 7
+# define PARAM	 8
 
 typedef struct s_input
 {
@@ -52,7 +52,7 @@ typedef struct s_array
 	char	**cmds;
 	char	***envp;
 	int		*type;
-	int 	*pipe;
+	int		*pipe;
 }				t_array;
 
 typedef struct s_command
@@ -79,62 +79,62 @@ typedef struct s_execute {
 	t_command	**token;
 }	t_execute;
 
-int		main(int argc, char **argv, char **envp);
-int		input_sort(char *line, char ***envp, int *l_r);
-void	mod_lstadd_back(t_input **lst, t_input *new);
-t_input	*mod_lstlast(t_input *lst);
-int		input_type(char *s, char *s_one, char **envp);
-int		is_env_var(char *s);
-int		is_built_in(char *s);
-int		is_pipe(char *s);
-int		is_red(char s);
-int		is_file(char *s);
-int		sort_array(t_input **input, t_array *array);
+int			main(int argc, char **argv, char **envp);
+int			input_sort(char *line, char ***envp, int *l_r);
+void		mod_lstadd_back(t_input **lst, t_input *new);
+t_input		*mod_lstlast(t_input *lst);
+int			input_type(char *s, char *s_one, char **envp);
+int			is_env_var(char *s);
+int			is_built_in(char *s);
+int			is_pipe(char *s);
+int			is_red(char s);
+int			is_file(char *s);
+int			sort_array(t_input **input, t_array *array);
 t_command	**get_commands(t_array **array);
-int		nv_after_red(t_input **input, int i);
-char	*mod_get_env(char **envp, char *string, int j, char *s);
-char	**mod_split(char *s, char c);
-void	mod_countsplitting(char *s, int *j, char c);
-int		mod_possplit(const char *s, char c);
-char	*mod_strjoin(char *s1, char *s2);
-char	*mod_nofree_strjoin(char *s1, char *s2);
-char	*modified_strjoin(char *s1, char *s2);
-char	**dup_array(char **array);
-void	free_array(char **array);
-void	free_list(t_input **input);
-int		ft_array_size(char **array);
-int		ft_echo(char *args);
-int		ft_env(char **envp);
-int		ft_export(char ***envp, char *string);
-char	**ft_append_string_to_array(char **array, char *string);
-int		ft_pwd(void);
-int		ft_unset(char ***envp, char *string);
-int		execute(char ***envp, t_command **token);
-void	ft_exit(char **array, t_execute *exec);
-char	*ft_get_env(char **envp, char *string);
-int		ft_cd(char *command, char ***envp);
-void	print_list(t_input **input);
-void	print_cmds(char **s, int *type, int *pipe);
-void	print_commands(t_command **token);
-char	**ft_get_commands(t_execute *new, char **parsed);
-char	*ft_check_command_and_get_path(char *command, char **envp);
-void	ft_free_array(char **array);
-char	**ft_special_split(char const *s, char c);
-char	**ft_get_command_arg_array(char *command);
-void	ft_remove(char *start, char *end);
-void	ft_close_fds(t_execute *exec, int current_child);
-void	ft_close_all_fds(t_execute *exec);
-int		ft_print_command_error(char **parsed, int error_code, int i);
-int		ft_init_struct(t_execute *new, t_command **token, char ***envp);
-void	ft_free_data(t_execute *exec);
-int		ft_forking(t_execute *exec);
-int		ft_child(int i, t_execute *exec);
-int		ft_parent(t_execute *exec);
-char	*ft_remove_slash(char *deleted);
-int		ft_strchr_count(const char *string, char c);
-int		ft_init(t_execute *exec, t_command **token, char ***envp);
-int		ft_free_end(int ret, char **array, t_execute *exec);
-// int		*ft_get_types_commands(t_execute *new);
-int		ft_set_redirects(t_execute *exec, int i);
+int			nv_after_red(t_input **input, int i);
+char		*mod_get_env(char **envp, char *string, int j, char *s);
+char		**mod_split(char *s, char c);
+void		mod_countsplitting(char *s, int *j, char c);
+int			mod_possplit(const char *s, char c);
+char		*mod_strjoin(char *s1, char *s2);
+char		*mod_nofree_strjoin(char *s1, char *s2);
+char		*modified_strjoin(char *s1, char *s2);
+char		**dup_array(char **array);
+void		free_array(char **array);
+void		free_list(t_input **input);
+int			ft_array_size(char **array);
+int			ft_echo(char *args);
+int			ft_env(char **envp);
+int			ft_export(char ***envp, char *string);
+char		**ft_append_string_to_array(char **array, char *string);
+int			ft_pwd(void);
+int			ft_unset(char ***envp, char *string);
+int			execute(char ***envp, t_command **token);
+void		ft_exit(char **array, t_execute *exec);
+char		*ft_get_env(char **envp, char *string);
+int			ft_cd(char *command, char ***envp);
+void		print_list(t_input **input);
+void		print_cmds(char **s, int *type, int *pipe);
+void		print_commands(t_command **token);
+char		**ft_get_commands(t_execute *new, char **parsed);
+char		*ft_check_command_and_get_path(char *command, char **envp);
+void		ft_free_array(char **array);
+char		**ft_special_split(char const *s, char c);
+char		**ft_get_command_arg_array(char *command);
+void		ft_remove(char *start, char *end);
+void		ft_close_fds(t_execute *exec, int current_child);
+void		ft_close_all_fds(t_execute *exec);
+int			ft_print_command_error(char **parsed, int error_code, int i);
+int			ft_init_struct(t_execute *new, t_command **token, char ***envp);
+void		ft_free_data(t_execute *exec);
+int			ft_forking(t_execute *exec);
+int			ft_child(int i, t_execute *exec);
+int			ft_parent(t_execute *exec);
+char		*ft_remove_slash(char *deleted);
+int			ft_strchr_count(const char *string, char c);
+int			ft_init(t_execute *exec, t_command **token, char ***envp);
+int			ft_free_end(int ret, char **array, t_execute *exec);
+// int			*ft_get_types_commands(t_execute *new);
+int			ft_set_redirects(t_execute *exec, int i);
 
 #endif
