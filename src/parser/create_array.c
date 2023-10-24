ba5f7ee23c662c5f0f73e1f8770d7c95f67ae71e
nschutz <nschutz@student.42wolfsburg.de>
2023-10-24 12:39:53 +0200

    feat: making create_array norminette freundlich by splitting it in 2 files v2

--- src/parser/create_array.c
+++ src/parser/create_array.c
@@ -50,62 +50,6 @@ static char	*is_cmd(t_input **input, t_array array, int count)
 	return (tmp);
 }
 
-static int	type_redirect(t_input **input, t_array array, int *count)
-{
-	int	i;
-
-	i = 2;
-	if (!ft_strncmp((*input)->word, "<", 2))
-		array.type[*count] = 1;
-	else if (!ft_strncmp((*input)->word, "<<", 3))
-		array.type[*count] = 2;
-	else if (!ft_strncmp((*input)->word, ">", 2))
-		array.type[*count] = 3;
-	else if (!ft_strncmp((*input)->word, ">>", 3))
-		array.type[*count] = 4;
-	else
-	{
-		perror("Invalid redirect");
-		i = 0;
-	}
-	return (i);
-}
-
-static void	redirect_after_param(t_array array, int *count, char **tmp)
-{
-	if (*tmp != NULL)
-	{
-		array.cmds[*count] = *tmp;
-		*tmp = NULL;
-		(*count)++;
-		array.pipe[*count] = 0;
-	}
-}
-
-static int	fill_tmp(t_input **input, t_array array, int *count, char **tmp)
-{
-	int	i;
-
-	i = 0;
-	while (*input && (*input)->type != PIPE && i != 1)
-	{
-		if ((*input)->type == REDIRECT)
-		{
-			redirect_after_param(array, count, tmp);
-			i = type_redirect(input, array, count);
-		}
-		else
-		{
-			*tmp = mod_strjoin(*tmp, (*input)->word);
-			i--;
-		}
-		(*input) = (*input)->next;
-		if (nv_after_red(input, i) == 258)
-			return (258);
-	}
-	return (0);
-}
-
 int	sort_array(t_input **input, t_array	*array)
 {
 	t_command	**token;
