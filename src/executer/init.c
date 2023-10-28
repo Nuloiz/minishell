8754554cad890ff070fa86bfa6940b70679a01d3
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-10-28 09:25:18 +0200

    initializing new file count

--- src/executer/init.c
+++ src/executer/init.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:16:52 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/10/19 12:17:40 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/10/19 18:53:43 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ static void	ft_init_from_token(t_execute *new, t_command **token)
 			new->count_commands ++;
 		if (token[i]->type == 6)
 			new->count_builtins ++;
+		if (token[i]->type >= 1 && token[i]->type <= 4)
+			new->count_only_files ++;
 	}
 }
 
@@ -34,8 +36,9 @@ int	ft_init_struct(t_execute *new, t_command **token, char ***envp)
 	new->count_commands = 0;
 	new->count_builtins = 0;
 	new->count_limiter = 0;
+	new->count_only_files = 0;
 	ft_init_from_token(new, token);
-	new->count_children = new->count_commands + new->count_builtins;
+	new->count_children = new->count_commands + new->count_builtins + new->count_only_files;
 	new->id = malloc(sizeof(int) * (new->count_children));
 	new->count_pipes = new->count_children - 1;
 	if (token[0]->limiter)
