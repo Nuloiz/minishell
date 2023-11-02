77f045c65b42e931185ffeca8d5c952b56ad981b
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-11-02 11:24:20 +0100

    outsourced echoctl function and added handle prompt and executing function

--- src/signals.c
+++ src/signals.c
@@ -6,64 +6,64 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 17:36:43 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/11/01 18:58:57 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/11/02 11:15:56 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// turn off ECHOCTL flag
-void	turn_off_ctl_echo(void)
-{
-	struct termios	term;
-
-	if (isatty(STDIN_FILENO))
-	{
-		tcgetattr(STDIN_FILENO, &term);
-		term.c_lflag = term.c_lflag & ~ECHOCTL;
-		tcsetattr(STDIN_FILENO, TCSANOW, &term);
-	}
-	else if (isatty(STDOUT_FILENO))
-	{
-		tcgetattr(STDOUT_FILENO, &term);
-		term.c_lflag = term.c_lflag & ~ECHOCTL;
-		tcsetattr(STDOUT_FILENO, TCSANOW, &term);
-	}
-	else if (isatty(STDERR_FILENO))
-	{
-		tcgetattr(STDERR_FILENO, &term);
-		term.c_lflag = term.c_lflag & ~ECHOCTL;
-		tcsetattr(STDERR_FILENO, TCSANOW, &term);
-	}
-}
-
-// resets global and return exit
-int	reset_signal(int exit_status)
-{
-	int	temp;
-
-	temp = g_signal;
-	g_signal = 0;
-	if (!exit_status)
-		return (exit_status);
-	return (temp);
-}
-
-void	ft_sig_handle(int sig)
+void	ft_sig_handle_prompt(int sig)
 {
 	if (sig == 2)
 	{
-		g_signal = 1;
+		g_signal = 2;
 		rl_replace_line("", 0);
 		rl_on_new_line();
 		write(STDOUT_FILENO, "\n", 1);
 		rl_redisplay();
-		
-		// readline, rl_clear_history, rl_on_new_line,
-		// rl_replace_line, rl_redisplay
+	}
+	else if (sig == 3)
+	{
+		g_signal = 3;
+		printf("Quit: 3\n");
+	}
+}
+
+void	ft_sig_handle_execute(int sig)
+{
+	if (sig == 2)
+		g_signal = 2;
+	else if (sig == 3)
+	{
+		g_signal = 3;
+		printf("Quit: 3\n");
+	}
+}
+
+void	sig_handle_executer(int sig)
+{
+	if (sig == 2)
+	{
+		g_signal = 2;
 	}
 	else if (sig == 3)
 		g_signal = 3;
 	else
 		printf("signal: %i caught!\n", sig);
+}
+
+void	set_sig_handle_executer(struct sigaction *sa)
+{
+	sa->sa_handler = &ft_sig_handle_execute;
+	sa->sa_flags = SA_RESTART;
+	sigaction(SIGINT, sa, NULL);
+	sigaction(SIGQUIT, sa, NULL);
+}
+
+void	set_sig_handle_prompt(struct sigaction *sa)
+{
+	sa->sa_handler = &ft_sig_handle_prompt;
+	sa->sa_flags = SA_RESTART;
+	sigaction(SIGINT, sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
 }
