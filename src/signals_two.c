ce2c84f3a0a8253f818811965150278b9af8fd08
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-11-02 11:16:52 +0100

    added file

---
+++ src/signals_two.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals_two.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 11:15:38 by dnebatz           #+#    #+#             */
+/*   Updated: 2023/11/02 11:16:00 by dnebatz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// turn off ECHOCTL flag
+void	turn_off_ctl_echo(void)
+{
+	struct termios	term;
+
+	if (isatty(STDIN_FILENO))
+	{
+		tcgetattr(STDIN_FILENO, &term);
+		term.c_lflag = term.c_lflag & ~ECHOCTL;
+		tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	}
+	else if (isatty(STDOUT_FILENO))
+	{
+		tcgetattr(STDOUT_FILENO, &term);
+		term.c_lflag = term.c_lflag & ~ECHOCTL;
+		tcsetattr(STDOUT_FILENO, TCSANOW, &term);
+	}
+	else if (isatty(STDERR_FILENO))
+	{
+		tcgetattr(STDERR_FILENO, &term);
+		term.c_lflag = term.c_lflag & ~ECHOCTL;
+		tcsetattr(STDERR_FILENO, TCSANOW, &term);
+	}
+}
