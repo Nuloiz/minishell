bf2ca2bd0bc7110bfdbcd3df9ebb33441a2fd85f
nschutz <nschutz@student.42wolfsburg.de>
2023-11-03 12:18:50 +0100

    feat: fixing '$PATH'

--- src/parser/input_sort.c
+++ src/parser/input_sort.c
@@ -57,13 +57,13 @@ static char	*quotes(char *s, char c)
 	return (ret);
 }
 
-static t_input	*found_quote(t_input *new, char **s)
+static t_input	*found_quote(t_input *new, char **s, char c)
 {
 	char 	*tmp;
 	int 	i;
 
 	i = 0;
-	while ((*s)[i] && (*s)[i] != 34 && (*s)[i] != 39)
+	while ((*s)[i] && (*s)[i] != c)
 		i++;
 	tmp = ft_substr(*s, 0, i);
 	if (!tmp && i > 0)
@@ -82,23 +82,31 @@ static t_input	*found_quote(t_input *new, char **s)
 }
 static t_input	*new_node(char **s, char **envp, int l_r)
 {
+	int		i;
+	int		j;
 	t_input	*new;
 
+	i = 0;
+	j = 0;
 	new = ft_calloc(1, sizeof(t_input));
 	if (!new)
 		return (NULL);
 	new->type = input_type(*s, envp);
 	if (ft_strchr(*s, 39) || ft_strchr(*s, 34))
 	{
-		new = found_quote(new, s);
+		while (*s[i] != 39 && *s[i] != 34)
+			i++;
+		if (*s[i] == 39)
+			j = 1;
+		new = found_quote(new, s, *s[i]);
 		if (!new)
 			return (NULL);
 	}
 	else
 		new->word = *s;
-	if (new->type == 4)
+	if (new->type == 4 && j == 0)
 	{
-		new->word = env_var(new->word, envp, l_r); //leaks
+		new->word = env_var(new->word, envp, l_r);
 		if (!new->word)
 			return (NULL);
 	}
@@ -119,7 +127,7 @@ static t_input	**linked_list_start(char **cmd, char **envp, \
 		num++;
 	while (i < num)
 	{
-		new = new_node (&cmd[i], envp, l_r); //leaks
+		new = new_node (&cmd[i], envp, l_r);
 		if (!new)
 			return (NULL);
 		mod_lstadd_back(input, new);
