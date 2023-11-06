5b4a132563a704cff14bb932649117ee596d30a0
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-11-06 06:59:19 +0100

    fixed memory leak spllited not freed

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*   By: dnebatz <dnebatz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 08:41:17 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/11/03 14:32:13 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/11/06 06:57:17 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ void	ft_exit(t_execute *exec, char *args)
 					("minishell: exit: a: numeric argument required\n", 2);
 		}
 	}
+	free_array(splitted);
 	free_token(exec->token);
 	ft_free_array(*exec->envp);
 	ft_close_all_fds(exec);
