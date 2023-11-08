7d9cd2a0e95e6a2e6c63c3efecf8279359c34b98
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-11-08 12:52:53 +0100

    norm changes:exported functions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:02:33 by nschutz           #+#    #+#             */
-/*   Updated: 2023/11/06 19:15:53 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/11/08 12:51:31 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,51 @@
 
 int	g_signal;
 
+static char	**init_main(int *last_return, char **envp, int *argc, char **argv)
+{
+	char				**new_env;
+
+	set_sig_handle_prompt();
+	turn_off_ctl_echo();
+	last_return = 0;
+	g_signal = 0;
+	(void) argc;
+	(void) argv;
+	new_env = dup_array(envp);
+	if (!new_env)
+		return (NULL);
+	else
+		return (new_env);
+}
+
+static void	free_main(char **new_env, char *line)
+{
+	free_array(new_env);
+	free(line);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char				**new_env;
 	char				*line;
 	int					last_return;
 
-	set_sig_handle_prompt();
-	turn_off_ctl_echo();
-	g_signal = 0;
-	(void) argc;
-	(void) argv;
-	new_env = dup_array(envp);
-	if (!new_env)
-		return (-1);
-	last_return = 0;
+	new_env = init_main(&last_return, envp, &argc, argv);
 	while (1)
 	{
 		line = readline("minishell: ");
 		if (!line)
 		{
-			// rl_replace_line("minishell: exit", 0);
 			printf("exit\n");
-			free_array(new_env);
-			free(line);
+			free_main(new_env, line);
 			break ;
 		}
 		add_history(line);
-		// set_sig_handle_executer(&sa);
 		set_sig_handle_executer();
 		last_return = input_sort(line, &new_env, last_return);
 		if (last_return == -1)
-		{
-			free_array(new_env);
-			free(line);
-			return (-1);
-		}
+			return (free_main(new_env, line), -1);
 		g_signal = 0;
-		// set_sig_handle_prompt(&sa);
 		set_sig_handle_prompt();
 		free(line);
 	}
