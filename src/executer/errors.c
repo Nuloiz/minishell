1d1f9845e2a4ffa25ec86d8d7a22200cdcf303c6
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-11-09 10:39:40 +0100

    removed exit and added return

--- src/executer/errors.c
+++ src/executer/errors.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 16:56:52 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/11/01 10:24:56 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/11/09 10:34:30 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_print_command_error(char *parsed, int error_code)
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(parsed, 2);
 	ft_putstr_fd(": command not found\n", 2);
-	exit (error_code);
+	return (error_code);
 }
