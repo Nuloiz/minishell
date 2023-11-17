f24aabc887adff362763222f86b4409dbc4e3ada
nschutz <nschutz@student.42wolfsburg.de>
2023-11-17 11:01:55 +0100

    feat fixing small malloc leak in get_env_in_quotes

--- src/parser/new_quotes.c
+++ src/parser/new_quotes.c
@@ -28,13 +28,17 @@ static char *string_vor_quote(char *s, int l_r, char **envp)
 static char *get_env_in_quotes(char *s, int *i, t_quote info)
 {
 	char	*tmp;
+	char	*str;
 	int		j;
 
 	j = *i;
 	while (s[*i] && s[*i] != info.c)
 		(*i)++;
-	tmp = env_var(ft_substr(s, j, *i - j), info.envp, info.l_r);
-	return (tmp);
+	str = ft_substr(s, j, *i - j);
+	if (!str)
+		return (NULL);
+	tmp = env_var(str, info.envp, info.l_r);
+	return (free(str), tmp);
 }
 
 static char *more_env(char *s, int *i, int *j, t_quote info)
